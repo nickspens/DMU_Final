de4e945344ae4cd7779fe43a343d9f147e8a9dd3
Nick S <nicholas.spens@colorado.edu>
2022-05-03 14:10:43 -0600

    Solver running, not tuned

--- Environment/SolverTest.jl
+++ Environment/SolverTest.jl
@@ -1,3 +1,4 @@
+
 using LinearAlgebra
 using Test
 using ReinforcementLearningBase.RLBase
@@ -195,8 +196,63 @@ end
 
 using ReinforcementLearning
 
-env = SpacecraftEnv()
+#env = SpacecraftEnv()
 #RLBase.test_runnable!(env)
-run(RandomPolicy(),env,StopAfterStep(1000),TotalRewardPerEpisode())
+#run(RandomPolicy(),env,StopAfterStep(1000),TotalRewardPerEpisode())
 
 
+#=======================================================================================================================================#
+
+using ReinforcementLearning
+using StableRNGs
+using Flux
+using Flux.Losses
+
+function RL.Experiment(
+    ::Val{:JuliaRL},
+    ::Val{:BasicDQN},
+    ::Val{:SpacecraftEnv},
+    ::Nothing;
+    seed = 123,
+)
+    rng = StableRNG(seed)
+    env = SpacecraftEnv(; T = Float64, max_steps = 5000, rng = rng)
+    ns, na = length(state(env)), length(action_space(env))
+    agent = Agent(
+        policy = QBasedPolicy(
+            learner = BasicDQNLearner(
+                approximator = NeuralNetworkApproximator(
+                    model = Chain(
+                        Dense(ns, 64, relu; init = glorot_uniform(rng)),
+                        Dense(64, 64, relu; init = glorot_uniform(rng)),
+                        Dense(64, na; init = glorot_uniform(rng)),
+                    ) |> gpu,
+                    optimizer = ADAM(),
+                ),
+                batch_size = 32,
+                min_replay_history = 100,
+                loss_func = huber_loss,
+                rng = rng,
+            ),
+            explorer = EpsilonGreedyExplorer(
+                kind = :exp,
+                ϵ_stable = 0.01,
+                decay_steps = 500,
+                rng = rng,
+            ),
+        ),
+        trajectory = CircularArraySARTTrajectory(
+            capacity = 50_000,
+            state = Vector{Float64} => (ns,),
+        ),
+    )
+
+    stop_condition = StopAfterStep(70_000, is_show_progress=!haskey(ENV, "CI"))
+    hook = TotalRewardPerEpisode()
+
+    Experiment(agent, env, stop_condition, hook, "")
+end
+using Plots
+ex = E`JuliaRL_BasicDQN_SpacecraftEnv`
+run(ex)
+plot(ex.hook.rewards)
